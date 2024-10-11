f709e9495b6a0f564dbf468a44d89de1cfd26712
Victor Jarvis <vctrjrvs@gmail.com>
2024-10-11 18:41:00 -0500

    started mod6, start next on page 21, need to fix images

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,30 @@
+export const trips = [
+    {
+        "code":"GALR210214",
+        "name": "Gale Reef",
+        "length":"4 nights / 5 days",
+        "start":"210214",
+        "resort":"Emerald Bay, 3 stars",
+        "perPerson":"799.00",
+        "image":"../images/reef1.jpg",
+        "description":"Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis."
+    },{
+        "code":"DAWR210315",
+        "name": "Dawson's Reef",
+        "length":"4 nights / 5 days",
+        "start":"210315",
+        "resort":"Blue Lagoon, 4 stars",
+        "perPerson":"1199.00",
+        "image":"../images/reef2.jpg",
+        "description":"Integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl. Curabitur odio lacus, blandit ut hendrerit."
+    },{
+        "code":"CLAR210621",
+        "name": "Claire's Reef",
+        "length":"4 nights / 5 days",
+        "start":"210621",
+        "resort":"Coral Sands, 5 stars",
+        "perPerson":"1999.00",
+        "image":"../images/reef3.jpg",
+        "description":"Donec sed felis risus. Nulla facilisi. Donec a orci tellus, et auctor odio. Fusce ac orci nibh, quis semper arcu. Cras orci neque, euismod et accumsan ac, sagittis molestie lorem. Proin odio sapien, elementum at tempor non."
+    }
+]
---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { trips } from '../data/trips';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css'
+})
+export class TripListingComponent implements OnInit {
+  trips: Array<any> = trips;
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+}
